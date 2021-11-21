e3509ca7071bda4e47154ecf86808870d920ccdf
Alex Trujillo <trual4@gmail.com>
2021-11-21 13:33:29 -0700

    toggle button bug free

--- src/components/atoms/TodoList.jsx
+++ src/components/atoms/TodoList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { List } from '@material-ui/core';
 import Todo from './Todo';
 
-export default function TodoList({ todos, removeTodo }) {
+export default function TodoList({ todos, toggleComplete, removeTodo }) {
   return (
     <List>
       {todos.map(todo => (
-        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} /> 
+        <Todo key={todo && todo.id} todo={todo} toggleComplete={toggleComplete} removeTodo={removeTodo} /> 
       ))}
     </List>
   )
