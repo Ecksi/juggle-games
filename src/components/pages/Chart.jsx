003f621e7a0028950b882e47f894f9a5ca569525
Alex Trujillo <trual4@gmail.com>
2021-11-21 18:31:28 -0700

    update chart page

--- src/components/pages/Chart.jsx
+++ src/components/pages/Chart.jsx
@@ -5,12 +5,25 @@ import { List } from '@material-ui/core';
 
 export default function Chart(props) {
 
-  const [threeBallTricks, setThreeBallTricks] = useState(jugglingTricks['3ball'])
+  const [allBallTricks, setAllBallTricks] = useState(jugglingTricks)
   return (
     <div>
+            <h3>3-ball tricks</h3>
       <List>
-      {threeBallTricks.map(trick => (
+      {allBallTricks['3ball'].map(trick => (
        <Trick trick={trick} numBalls={'3ball'} />
+      ))}
+    </List>
+    <h3>4-ball tricks</h3>
+    <List>
+      {allBallTricks['4ball'].map(trick => (
+       <Trick trick={trick} numBalls={'4ball'} />
+      ))}
+    </List>
+    <h3>5-ball tricks</h3>
+    <List>
+      {allBallTricks['5ball'].map(trick => (
+       <Trick trick={trick} numBalls={'5ball'} />
       ))}
     </List>
     </div>
