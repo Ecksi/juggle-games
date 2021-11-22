2e6fd6450aeffd163f61a760cea622288eae76aa
Jim Ecksi <jimecksi@gmail.com>
2021-11-21 19:49:39 -0500

    Update Chart component page

--- src/components/pages/Chart.jsx
+++ src/components/pages/Chart.jsx
@@ -1,17 +1,52 @@
-import React, { useState } from "react"
-import jugglingTricks from "../../assets/JugglingTricks/JugglingTricks.json"
-import Trick from "../juggling/Trick";
+import React from "react"
+import { Link } from "react-router-dom";
+import tricks from "../../assets/JugglingTricks/JugglingTricks.json"
 
-export default function Chart(props) {
-
-  const [threeBallTricks, setThreeBallTricks] = useState(jugglingTricks['3ball'])
+export default function Chart() {
   return (
     <div>
-      <p>
-      {threeBallTricks.map(trick => (
-       <Trick trick={trick} />
+      <h3>3-ball tricks</h3>
+      {tricks['3ball'].map(trick => (
+        <Link
+          to={`/3ball/${trick.name}`}
+        >
+        <button 
+          style={{ 
+            height: 100,
+            width: 100,
+            cursor: 'pointer',
+            'border-radius': 50,
+          }}
+          onClick={() => console.log('hi')}
+        >{trick.name}</button>
+        </Link>
       ))}
-    </p>
+      <hr />
+      <h3>4-ball tricks</h3>
+      {tricks['4ball'].map(trick => (
+        <button 
+          style={{ 
+            height: 100,
+            width: 100,
+            cursor: 'pointer',
+            'border-radius': 50,
+          }}
+          onClick={() => console.log('hi')}
+        >{trick.name}</button>
+      ))}
+      <hr />
+      <h3>5-ball tricks</h3>
+      {tricks['5ball'].map(trick => (
+        <button 
+          style={{ 
+            height: 100,
+            width: 100,
+            cursor: 'pointer',
+            'border-radius': 50,
+          }}
+          onClick={() => console.log('hi')}
+        >{trick.name}</button>
+      ))}
     </div>
   );
 }
