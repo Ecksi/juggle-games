2b41775e6da83e69eddfc5f852623de4d298d70a
Alex Trujillo <trual4@gmail.com>
2021-11-21 20:07:35 -0700

    remove key warning

--- src/components/juggling/Trick.jsx
+++ src/components/juggling/Trick.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Trick({trick, numBalls}) {
 
     return (
-        <ListItem style= {{display: "flex"}}>
+        <ListItem key={numBalls + trick.id} style= {{display: "flex"}}>
             <Link to={`/jugglingTricks/${numBalls}/${trick.name}`}>
             <Button variant="contained">{trick.name}</Button>
             </Link>
