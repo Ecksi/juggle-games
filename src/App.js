03cc46b3d0b2b5e14db04404e23fa18e2235cae2
Jim Ecksi <jimecksi@gmail.com>
2021-12-08 08:16:50 -0500

    Add learn to juggle route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/molecules/NavBar";
-import Chart from "./components/pages/Chart";
 import Home from "./components/pages/Home";
+import AddTrick from "./components/pages/AddTrick";
+import Chart from "./components/pages/Chart";
+import LearnToJuggle from "./components/pages/LearnToJuggle";
 import Tasks from "./components/pages/Tasks";
-import AddTrick from "./components/pages/AddTrick.jsx"
 import TrickPage from "./components/pages/TrickPage";
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route exact path="/chart" element={<Chart />}></Route>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/tasks" element={<Tasks />}></Route>
-          <Route exact path="/addTrick" element={<AddTrick />}></Route>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/addTrick" element={<AddTrick />} />
+          <Route exact path="/chart" element={<Chart />} />
+          <Route exact path="/learnToJuggle" element={<LearnToJuggle />} />
+          <Route exact path="/tasks" element={<Tasks />} />
           <Route
             path="/jugglingTricks/:numBalls/:trickName"
             element={<TrickPage />}
