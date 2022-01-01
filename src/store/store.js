9681667eafc1917b513eeb2f1107ed5ad91c3605
Jim <ecksi@Jims-MacBook-Pro.local>
2022-01-01 16:42:49 -0500

    Rename store.js to index.js

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import jugglingTrickReducer from "./reducers/jugglingTrickSlice";
-
-export default configureStore({
-  reducer: {
-    jugglingTrick: jugglingTrickReducer,
-  },
-});
