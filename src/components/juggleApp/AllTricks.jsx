fd4ae887830d9709fc4f16f3e602bb42fd38c6d4
Jim <ecksi@Jims-MacBook-Pro.local>
2022-01-01 16:47:22 -0500

    Prettier code

--- src/components/juggleApp/AllTricks.jsx
+++ src/components/juggleApp/AllTricks.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchJugglingTricksData } from "../../store/reducers/jugglingTrickSlice";
-import { Accordion, AccordionDetails, AccordionSummary, Box, List, Typography } from "@mui/material";
+import { fetchJugglingTricksData } from "../../store/reducers/jugglingTricksSlice";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  List,
+  Typography,
+} from "@mui/material";
 import Trick from "./TrickListItem";
 import "./TrickApp.css";
 
-export default function AllTricks(props) {
+export default function AllTricks() {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,20 +20,25 @@ export default function AllTricks(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const threeBallTricks = useSelector((state) => state.jugglingTrick.threeBall);
-  const fourBallTricks = useSelector((state) => state.jugglingTrick.fourBall);
+  const threeBallTricks = useSelector((state) => {
+    return state.jugglingTricks.threeBall;
+  });
+  const fourBallTricks = useSelector((state) => state.jugglingTricks.fourBall);
+  const fiveBallTricks = useSelector((state) => state.jugglingTricks.fiveBall);
 
   // State management for option 2
   const [expanded, setExpanded] = useState(false);
 
-  const handleChange = (panel) => (event, isExpanded) => {
+  const handleChange = (panel) => (e, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
-  
+
   return (
     <Box className="all-tricks">
       <h1>OPTION 1</h1>
-      <p style={{marginBottom: 40+'px'}}>Accordions kept open between clicks - space between accordions</p>
+      <p style={{ marginBottom: 40 + "px" }}>
+        Accordions kept open between clicks - space between accordions
+      </p>
       <Accordion>
         <AccordionSummary>
           <Typography variant="h5">3-ball tricks</Typography>
@@ -58,15 +70,25 @@ export default function AllTricks(props) {
           <Typography variant="h5">5-ball tricks</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <span style={{marginTop: '25px'}}>ThErE r n0 tRicKs hErE~</span>
-          <img src="https://i.imgflip.com/1pi6nv.jpg" alt="derp" width= "100px" />
+          <List>
+            {!!fiveBallTricks &&
+              fiveBallTricks.map((trick, i) => (
+                <Trick key={i} trick={trick} numBalls={"fiveBall"} />
+              ))}
+          </List>
         </AccordionDetails>
       </Accordion>
 
       {/* OPTION 2 */}
       <h1>OPTION 2</h1>
-      <p style={{marginBottom: 40+'px'}}>Only 1 accordion open at a time - no gap between accordions</p>
-      <Accordion expanded={expanded === 'panel-1'} onChange={handleChange('panel-1')} disableGutters>
+      <p style={{ marginBottom: 40 + "px" }}>
+        Only 1 accordion open at a time - no gap between accordions
+      </p>
+      <Accordion
+        expanded={expanded === "panel-1"}
+        onChange={handleChange("panel-1")}
+        disableGutters
+      >
         <AccordionSummary>
           <Typography variant="h5">3-ball tricks</Typography>
         </AccordionSummary>
@@ -79,7 +101,11 @@ export default function AllTricks(props) {
           </List>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel-2'} onChange={handleChange('panel-2')} disableGutters>
+      <Accordion
+        expanded={expanded === "panel-2"}
+        onChange={handleChange("panel-2")}
+        disableGutters
+      >
         <AccordionSummary>
           <Typography variant="h5">4-ball tricks</Typography>
         </AccordionSummary>
@@ -92,13 +118,21 @@ export default function AllTricks(props) {
           </List>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel-3'} onChange={handleChange('panel-3')} disableGutters>
+      <Accordion
+        expanded={expanded === "panel-3"}
+        onChange={handleChange("panel-3")}
+        disableGutters
+      >
         <AccordionSummary>
           <Typography variant="h5">5-ball tricks</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <span style={{marginTop: '25px'}}>ThErE r n0 tRicKs hErE~</span>
-          <img src="https://i.imgflip.com/1pi6nv.jpg" alt="derp" width= "100px" />
+          <span style={{ marginTop: "25px" }}>ThErE r n0 tRicKs hErE~</span>
+          <img
+            src="https://i.imgflip.com/1pi6nv.jpg"
+            alt="derp"
+            width="100px"
+          />
         </AccordionDetails>
       </Accordion>
     </Box>
